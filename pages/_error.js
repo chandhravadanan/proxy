3d301ec0e2339b993743d6924824698ed389b2f0
chandhravadanan <chandhravadanan@gmail.com>
2019-11-18 11:06:22 +0530

    error req redirected

--- pages/_error.js
+++ pages/_error.js
@@ -1,4 +1,6 @@
+
 import React from 'react'
+var urlinfo = require('url')
 
 function Error({ statusCode }) {
   return (
@@ -10,9 +12,20 @@ function Error({ statusCode }) {
   )
 }
 
-Error.getInitialProps = ({ res, err }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
-  return { statusCode }
+Error.getInitialProps = ({ req, res, err }) => {
+  let path = req.url
+  let referer = req.headers.referer;
+  if(referer){
+      let info = urlinfo.parse(referer);
+      let protocol = info.protocol
+      let host = info.host
+      let redirectUrl = 'https://proxy.now.sh/fetch?uri='+protocol+'//'+host+path
+      console.log('redirected to '+ redirectUrl)
+      res.writeHead(302, {Location: redirectUrl});
+      res.end();
+      return new Promise((req, res)=>{})
+  }
+  return {}
 }
 
 export default Error
